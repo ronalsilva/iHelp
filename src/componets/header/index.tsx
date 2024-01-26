115aac5d9c2f129d6b3d0a5463820c8b03ddb19d
Ronald Junger <ronald@lockebio.com>
2024-01-25 20:58:42 -0500

    set location on header

--- src/componets/header/index.tsx
+++ src/componets/header/index.tsx
@@ -14,48 +14,55 @@ export const Header = () => {
     }
 
     return (
-        <section id="header">
-            <div className="wrapper">
+        <header id="headerBox">
+            <section id="header">
+                <div className="wrapper">
 
-                <div className="menuHeader">
-                    <div className="logoHeader">
-                        <a href="/"><h1>IHelp</h1></a>
-                    </div>
-                    <ul>
-                        <li><a href="/search">Find service</a></li>
-                        <li><a href="">Suport</a></li>
-                        <li><a href="">About us</a></li>
-                    </ul>
-                </div>
-
-                <div className="loginHeader">
-                    <a href="/createaccount">Create account</a>
-                    <a href="/login" className='loginButton'>Login</a>
-                </div>
-
-                <div className="mobileMenu">
-                    <div className="iconMenu" onClick={funcOpenMenu}>
-                        <img src={require('../../assets/image/menu-open.png')} alt="menu" />
-                    </div>
-
-                    <div id="menuContentMobile" className={mobileMenu}>
-                        <div className="closeMenu" onClick={funcOpenMenu}>
-                            <img src={require('../../assets/image/menu-close.png')} alt="close menu" />
-                        </div>
-                        
-                        <div className="logoHeaderMobile">
+                    <div className="menuHeader">
+                        <div className="logoHeader">
                             <a href="/"><h1>IHelp</h1></a>
                         </div>
-
                         <ul>
                             <li><a href="/search">Find service</a></li>
-                            <li><a href="#">Suport</a></li>
-                            <li><a href="#">About us</a></li>
-                            <li className='loginMobile'><a href="/login">Login in</a><a href="/createAccount">Create account</a></li>
+                            <li><a href="">Suport</a></li>
+                            <li><a href="">About us</a></li>
                         </ul>
                     </div>
+
+                    <div className="loginHeader">
+                        <a href="/createaccount">Create account</a>
+                        <a href="/login" className='loginButton'>Login</a>
+                    </div>
+
+                    <div className="mobileMenu">
+                        <div className="iconMenu" onClick={funcOpenMenu}>
+                            <img src={require('../../assets/image/menu-open.png')} alt="menu" />
+                        </div>
+
+                        <div id="menuContentMobile" className={mobileMenu}>
+                            <div className="closeMenu" onClick={funcOpenMenu}>
+                                <img src={require('../../assets/image/menu-close.png')} alt="close menu" />
+                            </div>
+                            
+                            <div className="logoHeaderMobile">
+                                <a href="/"><h1>IHelp</h1></a>
+                            </div>
+
+                            <ul>
+                                <li><a href="/search">Find service</a></li>
+                                <li><a href="#">Suport</a></li>
+                                <li><a href="#">About us</a></li>
+                                <li className='loginMobile'><a href="/login">Login in</a><a href="/createAccount">Create account</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <div id="setLocation">
+                <div className="locationContent">
+                    <p><span></span>Put your location</p>
                 </div>
             </div>
-        </section>
+        </header>
     )
 }
